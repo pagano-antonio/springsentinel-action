1af51284c8f4244f3e25a2c203dceee9cb843b93
Antonio Pagano <59290688+pagano-antonio@users.noreply.github.com>
2026-07-06 17:13:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,4 @@ echo "Running SpringSentinel..."
 
 mvn -B io.github.pagano-antonio:spring-sentinel-maven-plugin:audit
 
-echo "Generating PR comment..."
-
-node /scripts/generate-comment.js
-
 echo "Done."
